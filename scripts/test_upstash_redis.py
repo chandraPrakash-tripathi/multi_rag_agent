eb5510e87d68484788def14a25718f68b0524116
chandraPrakash-tripathi <tripathi.cp07@gmail.com>
2026-07-23 22:16:52 +0530

    changes

--- scripts/test_upstash_redis.py
+++
@@ -1,9 +0,0 @@
-from upstash_redis import Redis
-
-redis = Redis(
-    url="https://classic-lamb-172999.upstash.io",
-    token="gQAAAAAAAqPHAAIgcDEwNzZiOGI5MDhlNTY0ZmZjOGY5ZjIzYjZjMjAwZGExNQ",
-)
-
-redis.set("foo", "bar")
-value = redis.get("bar")
